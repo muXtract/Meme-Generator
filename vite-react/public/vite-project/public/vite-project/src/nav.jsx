3c54b90e2bf2256b138c9c3b603a6ad48b13916e
Vijay <nvijay461@gmail.com>
2023-03-15 18:32:02 +0530

    commited

--- vite-react/public/vite-project/public/vite-project/src/nav.jsx
+++ vite-react/public/vite-project/public/vite-project/src/nav.jsx
@@ -1,10 +1,11 @@
 import React from "react"
+import trollFace from "/src/assets/images/troll-face.png"
 export default function nav(){
   return (
     <div className="container">
       <nav className="flex  bg-purple-600 ">
         <div className="left  flex space-x-6 ">
-          <img src="./src/assets/images/troll-face.png" alt="troll-face" width={70} />
+          <img src={trollFace} alt="troll-face" width={70} />
           <h1 className=" heading text-lg mt-3 md:text-2xl font-bold text-white">Meme Generator
           </h1>
         </div>
