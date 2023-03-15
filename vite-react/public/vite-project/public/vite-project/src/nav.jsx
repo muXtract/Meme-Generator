b62ce4e3802945faac64f8588163c8516958fe86
Vijay <nvijay461@gmail.com>
2023-03-15 17:54:04 +0530

    cm

--- vite-react/public/vite-project/public/vite-project/src/nav.jsx
+++ vite-react/public/vite-project/public/vite-project/src/nav.jsx
@@ -4,7 +4,7 @@ export default function nav(){
     <div className="container">
       <nav className="flex  bg-purple-600 ">
         <div className="left  flex space-x-6 ">
-          <img src="/src/troll face.png" width={70} />
+          <img src="./src/troll-face.png" width={70} />
           <h1 className=" heading text-lg mt-3 md:text-2xl font-bold text-white">Meme Generator
           </h1>
         </div>
